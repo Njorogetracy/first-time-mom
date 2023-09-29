64f2bd4e386710c79e33896ce294a61f4b9ea27b
Hope Tracy <hnjoroge64@gmail.com>
2023-09-29 14:16:15 +0000

    Deploy

--- first-time-mom/settings.py
+++ first-time-mom/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')  # noqa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['first-time-mom.herokuapp.com', 'localhost']  # noqa
+ALLOWED_HOSTS = ['first-time-mom.herokuapp.com', 'localhost']
 
 
 # Application definition
