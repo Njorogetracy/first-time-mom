fb5a24ed9b6b4cc60049f774682d030302e2e3a6
Hope Tracy <hnjoroge64@gmail.com>
2023-09-29 14:20:20 +0000

    rename app and deploy

--- first-time-mom/asgi.py
+++ first-time-mom/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firsttimemom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first-time-mom.settings')
 
 application = get_asgi_application()
--- first-time-mom/wsgi.py
+++ first-time-mom/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firsttimemom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first-time-mom.settings')
 
 application = get_wsgi_application()
