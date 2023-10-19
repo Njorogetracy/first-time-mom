a09399a47f02a397dbaf585dcc25b4ea1ab18502
Hope Tracy <hnjoroge64@gmail.com>
2023-10-19 13:09:26 +0000

    build: display activities to logged in users

--- classes/urls.py
+++ classes/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('hub/', HubView.as_view(), name='hub'),
+    path('', IndexView.as_view(), name='index'),  # for users not registered
+    path('hub/', HubView.as_view(), name='hub'),  # homepage for loggedin
     path('activities/', ActivityListView.as_view(), name='activity_list'),
-    # path('accounts/signup/', CustomSignupView.as_view(), name='signup'),
-    # path('accounts/login/', CustomLoginView.as_view(), name='login'),
-    # path('accounts/logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- classes/views.py
+++ classes/views.py
@@ -1,11 +1,5 @@
 from django.views.generic import ListView, TemplateView
 from .models import Activity, Booking
-# from .forms import CustomUserCreationForm
-# from django.contrib.auth import login
-# from django.shortcuts import render, redirect
-# from allauth.account.views import SignupView, LoginView, LogoutView
-# from .login import CustomLoginForm
-# from django.urls import reverse_lazy
 
 
 class IndexView(TemplateView):
@@ -20,30 +14,3 @@ class ActivityListView(ListView):
     model = Activity
     template_name = 'activity_list.html'
     context_object_name = 'activities'
-
-
-# class CustomSignupView(SignupView):
-#     model = CustomUser
-#     form_class = CustomUserCreationForm
-#     template_name = 'accounts/custom_signup.html'
-#     success_url = reverse_lazy('login')
-
-#     def form_valid(self, form):
-#         user = form.save()
-#         login(self.request, user)
-#         return super().form_valid(form)
-
-
-# class CustomLoginView(LoginView):
-#     form_class = CustomLoginForm
-#     template_name = 'accounts/login.html'
-#     success_url = reverse_lazy('home')
-
-#     def form_valid(self, form):
-#         user = form.get_user()
-#         login(self.request, user)
-#         return super().form_valid(form)
-
-
-# class CustomLogoutView(LogoutView):
-#     template_name = 'accounts/logout.html'
