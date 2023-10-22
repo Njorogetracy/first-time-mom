8355c1e300ee6efa1e2a218f8691e977caf0c12f
Hope Tracy <hnjoroge64@gmail.com>
2023-10-22 20:53:47 +0000

    build: add navigation and footer styles

---
+++ static/assets/js/script.js
@@ -0,0 +1,11 @@
+// JavaScript for navigation toggle
+document.addEventListener('DOMContentLoaded', function () {
+    var navbar = document.querySelector('.navbar');
+
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    navbarToggler.addEventListener('click', function () {
+        navbar.classList.toggle('show');
+    });
+});
+
+
