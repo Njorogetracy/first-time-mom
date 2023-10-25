e76835eb1ccc77aa8b3b82463077ef6408197cec
Hope Tracy <hnjoroge64@gmail.com>
2023-10-25 08:08:16 +0000

    build: add whitenoise to requirements.txt

--- first-time-mom/settings.py
+++ first-time-mom/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'  # noqa
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
